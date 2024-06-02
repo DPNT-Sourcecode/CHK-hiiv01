24198bc6af3cb6615a91f149b0637b7b0070e0d9
sbx_user1051 <sbx_user1051@169.254.49.245>
2024-06-02 11:51:30 +0000

    Sun Jun 02 11:51:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,24 +26,12 @@ def checkout(skus):
     total_price: int = 0
 
     for product, count in product_counts.items():
-        product_price = product_prices[product]
         if product in product_offers:
-            offers = product_offers[product]
-            for offer in sorted(offers, reverse=True):
-                offer_count, offer_value = offer
-                while count >= offer_count:
-                    count -= offer_count
-                    if isinstance(offer_value, int):
-                        total_price += offer_value
-                    elif offer_value in product_counts and product_counts[offer_value] > 0:
-                        product_counts[offer_value] -= 1
-                        total_price += product_price
-                    else:
-                        break
-            total_price += count * product_price
+            
         else:
             total_price += count * product_prices[product]
     return total_price
+
 
 
 
